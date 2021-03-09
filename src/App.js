a2c20309a18c45b78b382d09c9ff1641295f05a8
AJ <alexanderjessediaz@gmail.com>
2021-03-09 14:08:01 -0700

    add login form and login logic to App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 import './App.css';
 
 const App = () => {
+    if(!localStorage.getItem('username')) return <LoginForm />
+
     return (
         <ChatEngine
             height="100vh"
             projectID="fb84922d-4902-49e4-9e69-1811671e280e"
-            userName="Jackie"
-            userSecret="123"
+            userName={localStorage.getItem('username')}
+            userSecret={localStorage.getItem('password')}
             renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
         />
     );
