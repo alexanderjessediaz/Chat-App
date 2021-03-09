92d43679143703973ea2de2126b6e6c24e0dedc2
AJ <alexanderjessediaz@gmail.com>
2021-03-09 12:11:27 -0700

    download and import chat engine and styling, create admin user

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="fb84922d-4902-49e4-9e69-1811671e280e"
+            userName="Jackie"
+            userSecret="123"
+        />
+    );
+}
+
+export default App;
